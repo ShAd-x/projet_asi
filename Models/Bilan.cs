d66c7cbb142288d91fa04cd1b4c4d7410a9baf59
Adrien222 <adrienchretien2002@gmail.com>
2024-03-19 21:55:48 +0100

    Messages d'erreurs bilan

--- Models/Bilan.cs
+++ Models/Bilan.cs
@@ -2,24 +2,27 @@
 
 namespace Projet_ASI.Models
 {
-	public class Bilan
-	{
-		public int Id { get; set; }
-		[Required]
-		[Display(Name = "Date de la consultation")]
-		[DataType(DataType.Date)]
-		public DateTime DateConsultation { get; set; }
-		[Required]
-		[Display(Name = "Heure de la consultation")]
-		[DataType(DataType.Time)]
-		public DateTime HeureConsultation { get; set; }
-		[Required]
-		public string? MotifConsultation { get; set; }
-		[Required]
-		public string? NoteSupplementaire { get; set; }
+    public class Bilan
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La date de consultation est obligatoire.")]
+        [Display(Name = "Date de la consultation")]
+        [DataType(DataType.Date)]
+        public DateTime DateConsultation { get; set; }
+
+        [Required(ErrorMessage = "L'heure de consultation est obligatoire.")]
+        [Display(Name = "Heure de la consultation")]
+        [DataType(DataType.Time)]
+        public DateTime HeureConsultation { get; set; }
+
+        [Required(ErrorMessage = "Le motif de consultation est obligatoire.")]
+        public string? MotifConsultation { get; set; }
+
+        [Required(ErrorMessage = "La note supplémentaire est obligatoire.")]
+        public string? NoteSupplementaire { get; set; }
 
         public int? PatientId { get; set; }
         public Patient? Patient { get; set; }
-
-	}
+    }
 }
